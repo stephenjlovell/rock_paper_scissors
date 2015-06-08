60f4dc82f948b894cc6d53c3804f66fcd4294987
Steve Lovell <sjlovell34@gmail.com>
2015-06-08 00:42:14 -0400

    inject rulesets into Game class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,18 @@
 class HomeController < ApplicationController
 
+  before_action :new_game
+
   def show
   end
 
   def throw
-    @game = Game.new(params)
+    @game.play(params[:sign])
     render "/home/game_over"
   end
 
+  private
+
+  def new_game
+    @game = Game.new(rules: RPS_RULES)
+  end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,24 @@
 
   RPS_RULES = {
-    rock: :scissors,
-    paper: :rock,
-    scissors: :paper
+    rock: [:scissors],
+    paper: [:rock],
+    scissors: [:paper]
   }
+
+  RPSLS_RULES = {
+    rock: [:scissors, :lizzard],
+    paper: [:rock, :spock],
+    scissors: [:paper, :lizzard],
+    lizzard: [:paper, :spock],
+    spock: [:rock, :scissors]
+  } 
 
   class Game
 
-    attr_reader :player_throw, :opponent_throw, :result
+    attr_reader :player_throw, :opponent_throw, :winning_throw, :result
 
     def initialize(opts = {})
-      @player_throw = opts[:sign].to_sym
-      @result = result(@player_throw)
+      @rules = opts[:rules]
     end
 
     def message
@@ -25,17 +32,27 @@
       end
     end
 
-    private
+    def valid_signs
+      @rules.keys.map(&:to_s)
+    end
 
-    def result(player_throw)
-      @opponent_throw = [:rock, :paper, :scissors][rand(3)]
-      if player_throw == @opponent_throw
+    def play(player_throw)
+      @player_throw = player_throw.to_sym
+      @opponent_throw = @rules.keys.sample
+      @result = if @player_throw == @opponent_throw
         :tie
-      elsif RPS_RULES[player_throw] == @opponent_throw
+      elsif player_wins?(@player_throw, @opponent_throw)
+        @winning_throw = @player_throw
         :win
-      else 
+      else
+        @winning_throw = @opponent_throw 
         :lose
       end
     end
 
+    private
+
+    def player_wins?(player_throw, opponent_throw)
+      @rules[player_throw].include?(opponent_throw)
+    end
   end
